a6b377eca86b5e23b96137c6f7fd5008189d5e99
geunwoo shim <geunwoo.j.shim@gmail.com>
2020-05-06 10:35:13 +0900

    merge sh

--- create-registry.sh
+++ create-registry.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+openssl req -x509 -nodes -newkey rsa:4096 -keyout tls.key -out tls.crt -days 365 \
+ -config tls.req -extensions v3_req
+mkdir -p /etc/docker/certs.d/192.168.1.10:8443
+cp -f tls.crt /etc/docker/certs.d/192.168.1.10:8443
+
 yum -y install sshpass
 mkdir -p /etc/docker/certs.d/192.168.1.10:8443
 cp -f tls.crt /etc/docker/certs.d/192.168.1.10:8443
@@ -11,3 +16,15 @@ mkdir -p /etc/docker/certs.d/192.168.1.10:8443
 sshpass -pvagrant scp tls.crt w1-k8s:/etc/docker/certs.d/192.168.1.10:8443
 sshpass -pvagrant scp tls.crt w2-k8s:/etc/docker/certs.d/192.168.1.10:8443
 sshpass -pvagrant scp tls.crt w3-k8s:/etc/docker/certs.d/192.168.1.10:8443
+
+mkdir -p /data
+docker run -d \
+  --restart=always \
+  --name registry \
+  -v /root/create-registry:/certs:ro \
+  -v /data:/var/lib/registry \
+  -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
+  -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/tls.crt \
+  -e REGISTRY_HTTP_TLS_KEY=/certs/tls.key \
+  -p 8443:443 \
+  registry:2
