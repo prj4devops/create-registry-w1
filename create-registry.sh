06a3b5f60d27406f2494d1ca09357e93ab8dfc02
geunwoo shim <geunwoo.j.shim@gmail.com>
2020-05-06 10:51:22 +0900

    tls

--- create-registry.sh
+++ create-registry.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 openssl req -x509 -nodes -newkey rsa:4096 -keyout tls.key -out tls.crt -days 365 \
  -config tls.req -extensions v3_req
-mkdir -p /etc/docker/certs.d/192.168.1.10:8443
-cp -f tls.crt /etc/docker/certs.d/192.168.1.10:8443
 
 yum -y install sshpass
 mkdir -p /etc/docker/certs.d/192.168.1.10:8443
