8daf5a4feb21f3ac159f7b922dc32b4a2c0f33a1
GNU Shim <geunwoo.j.shim@gmail.com>
2020-06-07 12:20:26 +0900

    Update create-registry.sh

--- create-registry.sh
+++ create-registry.sh
@@ -3,23 +3,23 @@ openssl req -x509 -nodes -newkey rsa:4096 -keyout tls.key -out tls.crt -days 365
  -config tls.req -extensions v3_req
 
 yum -y install sshpass
-mkdir -p /etc/docker/certs.d/192.168.1.10:8443
-cp -f tls.crt /etc/docker/certs.d/192.168.1.10:8443
-sshpass -pvagrant ssh -o StrictHostKeyChecking=no root@w1-k8s \
-mkdir -p /etc/docker/certs.d/192.168.1.10:8443
+mkdir -p /etc/docker/certs.d/192.168.1.101:8443
+cp -f tls.crt /etc/docker/certs.d/192.168.1.101:8443
+sshpass -pvagrant ssh -o StrictHostKeyChecking=no root@m-k8s \
+mkdir -p /etc/docker/certs.d/192.168.1.101:8443
 sshpass -pvagrant ssh -o StrictHostKeyChecking=no root@w2-k8s \
-mkdir -p /etc/docker/certs.d/192.168.1.10:8443
+mkdir -p /etc/docker/certs.d/192.168.1.101:8443
 sshpass -pvagrant ssh -o StrictHostKeyChecking=no root@w3-k8s \
-mkdir -p /etc/docker/certs.d/192.168.1.10:8443
-sshpass -pvagrant scp tls.crt w1-k8s:/etc/docker/certs.d/192.168.1.10:8443
-sshpass -pvagrant scp tls.crt w2-k8s:/etc/docker/certs.d/192.168.1.10:8443
-sshpass -pvagrant scp tls.crt w3-k8s:/etc/docker/certs.d/192.168.1.10:8443
+mkdir -p /etc/docker/certs.d/192.168.1.101:8443
+sshpass -pvagrant scp tls.crt m-k8s:/etc/docker/certs.d/192.168.1.101:8443
+sshpass -pvagrant scp tls.crt w2-k8s:/etc/docker/certs.d/192.168.1.101:8443
+sshpass -pvagrant scp tls.crt w3-k8s:/etc/docker/certs.d/192.168.1.101:8443
 
 mkdir -p /data
 docker run -d \
   --restart=always \
   --name registry \
-  -v /root/create-registry:/certs:ro \
+  -v /root/create-registry-w1:/certs:ro \
   -v /data:/var/lib/registry \
   -e REGISTRY_HTTP_ADDR=0.0.0.0:443 \
   -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/tls.crt \
